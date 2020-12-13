0f1119e7c9a8f46a3af23602d8fbc6ffa9126e35
Marcos Loureiro <mloureiro.dev@gmail.com>
2020-12-13 15:38:01 +0100

    Add day 04 part 2

--- 2020/04.js
+++ 2020/04.js
@@ -28,7 +28,36 @@ const firstChallenge = data => {
 }
 
 const secondChallenge = data => {
-	return null
+	const isStringNumber = str => !isNaN(str)
+	const isNumberBetween = (str, min, max) => {
+		const num = parseInt(str, 10)
+
+		return isStringNumber(str) && min <= num && num <= max
+	}
+
+	const REQUIRED_FIELDS_VALIDATION = {
+		byr: v => isNumberBetween(v, 1920, 2002),
+		iyr: v => isNumberBetween(v, 2010, 2020),
+		eyr: v => isNumberBetween(v, 2020, 2030),
+		hgt: v => {
+			const [, height, unit] = (v && v.match(/^(\d+)(in|cm)$/)) || []
+
+			return Boolean(height && unit && (
+				(unit === 'cm' &&  isNumberBetween(height, 150, 193))
+				|| (unit === 'in' && isNumberBetween(height, 59, 76))
+			))
+		},
+		hcl: v => v && /^#[\da-f]{6}$/i.test(v),
+		ecl: v => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(v),
+		pid: v => v && /^\d{9}$/.test(v),
+	}
+
+	const isDocumentValid = document =>
+		Object.keys(REQUIRED_FIELDS_VALIDATION)
+			.every(field => REQUIRED_FIELDS_VALIDATION[field](document[field]))
+
+
+	return parseValidateAndCountDocuments(data, isDocumentValid)
 }
 
 console.log(`
