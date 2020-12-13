f239be32be10a9ebf7dfd77f2ac4992fb0fb237a
Marcos Loureiro <mloureiro.dev@gmail.com>
2020-12-13 01:39:51 +0100

    Define a template
    Default template to kickstart an exercise

---
+++ template.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const data = fs.readFileSync('./input.txt')
+    .toString()
+    .split('\n')
+    .filter(Boolean)
+
+const firstChallenge = data => {
+    return null
+}
+
+const secondChallenge = data => {
+    return null
+}
+
+console.log(`
+Result
+  1st: ${firstChallenge(data)}
+  2nd: ${secondChallenge(data)} 
+`)
