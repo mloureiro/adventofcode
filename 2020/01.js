4c091d7ff16a0e0c5064fcbce4b62bdc0826bd30
Marcos Loureiro <mloureiro.dev@gmail.com>
2020-12-13 15:37:50 +0100

    Add day 01 part 2

--- 2020/01.js
+++ 2020/01.js
@@ -18,6 +18,12 @@ const firstChallenge = data => {
 }
 
 const secondChallenge = data => {
+	for (let i = 0; i < data.length - 2; i++)
+		for (let j = i + 1; j < data.length - 1; j++)
+			for (let k = j + 1; k < data.length; k++)
+				if (data[i] + data[j] + data[k] === MATCH)
+					return data[i] * data[j] * data[k]
+
 	return null
 }
 
