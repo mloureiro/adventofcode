d7dc7f04f2a6ed289f552a385e9d2d1b1e6b8981
Marcos Loureiro <mloureiro.dev@gmail.com>
2020-12-13 15:38:17 +0100

    Add day 08 part 2

--- 2020/08.js
+++ 2020/08.js
@@ -43,7 +43,73 @@ const firstChallenge = data => {
 }
 
 const secondChallenge = data => {
-	return null
+	const interpretInstruction = instruction => {
+		const [action, value] = parseInstructions(instruction)
+		switch (action) {
+			case 'jmp':
+				return [value, 0]
+			case 'acc':
+				return [1, value]
+			case 'nop':
+				return [1, 0]
+			default:
+				throw Error(`Instruction "${instruction}" is invalid`)
+		}
+	}
+
+	const isInstructionSwappable = instruction =>
+		['jmp', 'nop'].includes(parseInstructions(instruction)[0])
+
+	const swapInstruction = instruction => {
+		const [action, value] = parseInstructions(instruction)
+		switch (action) {
+			case 'jmp': return `nop ${value}`
+			case 'nop': return `jmp ${value}`
+			default:
+				throw Error(`Instruction "${instruction}" not swappable`)
+		}
+	}
+
+	let visited = {}
+	let accumulator = 0
+	let position = 0
+	let history = []
+	let lastUnchangedPosition = null
+	while (position < data.length) {
+		let instruction = data[position]
+
+		if (visited[position]) {
+			if (lastUnchangedPosition)
+				history.splice(lastUnchangedPosition + 1, history.length - lastUnchangedPosition)
+
+			let foundBrokenInstruction = false
+			while (!foundBrokenInstruction) {
+				let currentInstruction
+				do {
+					const [prevPosition, prevAccumulator, prevInstruction, prevVisited] = history.pop()
+					position = prevPosition
+					accumulator = prevAccumulator
+					currentInstruction = prevInstruction
+					visited = prevVisited
+				} while (!isInstructionSwappable(currentInstruction))
+
+				instruction = swapInstruction(currentInstruction)
+				const [prevPosition] = interpretInstruction(instruction)
+
+				foundBrokenInstruction = visited[prevPosition]
+			}
+
+			lastUnchangedPosition = history.length - 1
+		}
+
+		visited[position] = true
+		history.push([position, accumulator, instruction, { ...visited }])
+		const [addPosition, addAccumulator] = interpretInstruction(instruction)
+		accumulator += addAccumulator
+		position += addPosition
+	}
+
+	return accumulator
 }
 
 console.log(`
