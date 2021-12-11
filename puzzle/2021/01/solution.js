008260e397730ed64b759225e88f7035b653894b
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-12-11 14:52:53 +0100

    Add 2021 day 01 resolution

---
+++ puzzle/2021/01/solution.js
@@ -0,0 +1,7 @@
+export const formatInput = input => input.split('\n').map(Number);
+
+export const part1 = d =>
+	d.reduce((a, c, i) => i !== 0 && c > d[i - 1] ? a+1 : a, 0);
+
+export const part2 = d =>
+	part1(d.map((c, i) => i+2 > d.length ? null : d[i] + d[i+1] + d[i+2]).slice(0,-2));
---
+++ puzzle/2021/01/tests.js
@@ -0,0 +1,17 @@
+const input = `
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+`.trim();
+
+export const tests = [
+	{ part: 1, input, result: 7 },
+	{ part: 2, input, result: 5 },
+];
