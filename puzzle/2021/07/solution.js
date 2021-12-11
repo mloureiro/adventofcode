5c58c1982228bd3ebfad471015d9419e50caa21d
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-12-11 15:59:36 +0100

    Add 2021 day 07 resolution

---
+++ puzzle/2021/07/solution.js
@@ -0,0 +1,33 @@
+export const formatInput = input => input.split(',').map(Number);
+
+export const part1 = input => {
+	const median = input.sort((a, b) => a < b ? -1 : a)[input.length / 2];
+	return input.reduce((a, v) => a + Math.abs(v - median) ,0)
+}
+
+export const part2 = input => {
+	const median = input.sort((a, b) => a < b ? -1 : a)[input.length / 2];
+	const mean = input.reduce((a, v) => a + v) / 2;
+
+	// ASSUMING mean IS SMALLER THAN median (works with our input)
+	let leastFuelSpent = Number.MAX_SAFE_INTEGER;
+	for (let i = median; i < mean; i++) {
+		const currentFuelSpent = input.reduce((a, v) => {
+			let total = 0
+			let difference = Math.abs(v - i);
+			for (let j = 1; j <= difference; j++) {
+				total += j
+			}
+
+			return a + total
+		} ,0)
+
+		if (currentFuelSpent < leastFuelSpent) {
+			leastFuelSpent = currentFuelSpent
+		} else {
+			return leastFuelSpent
+		}
+	}
+
+	return leastFuelSpent
+}
---
+++ puzzle/2021/07/tests.js
@@ -0,0 +1,6 @@
+const input = `16,1,2,0,4,2,7,1,2,14`;
+
+export const tests = [
+	{ part: 1, input, result: 37 },
+	{ part: 2, input, result: 168 },
+];
