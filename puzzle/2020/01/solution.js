6a0be4a7e880a7d570af9a79c93b8cf68421d120
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-12-11 18:29:43 +0100

    Convert 2020-01 to new template

--- puzzle/2020/01/solution.js
+++ puzzle/2020/01/solution.js
@@ -1,23 +1,17 @@
-const fs = require('fs')
-const data = fs.readFileSync('./01.txt')
-	.toString()
-	.split('\n')
-	.filter(Boolean)
-	.map(string => parseInt(string, 10))
+export const formatInput = input => input.split('\n').map(Number);
 
 const MATCH = 2020
 
-const firstChallenge = data => {
+export const part1 = data => {
 	for (let i = 0; i < data.length - 1; i++)
 		for (let j = i + 1; j < data.length; j++)
 			if (data[i] + data[j] === MATCH)
 				return data[i] * data[j]
 
-
 	return null
 }
 
-const secondChallenge = data => {
+export const part2 = data => {
 	for (let i = 0; i < data.length - 2; i++)
 		for (let j = i + 1; j < data.length - 1; j++)
 			for (let k = j + 1; k < data.length; k++)
@@ -26,9 +20,3 @@ const secondChallenge = data => {
 
 	return null
 }
-
-console.log(`
-Result
-  1st: ${firstChallenge(data)}
-  2nd: ${secondChallenge(data)}
-`)
---
+++ puzzle/2020/01/tests.js
@@ -0,0 +1,13 @@
+const input = `
+1721
+979
+366
+299
+675
+1456
+`.trim();
+
+export const tests = [
+	{ part: 1, input, result: 514579 },
+	{ part: 2, input, result: 241861950 },
+];
