2ed3176ce90be70876846ce8bfed5f9224a2bd14
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-12-11 14:48:01 +0100

    Fix typo in tests template

--- bin/puzzle-template/tests.js
+++ bin/puzzle-template/tests.js
@@ -3,6 +3,6 @@ const input = `
 `.trim();
 
 export const tests = [
-	{ part1: 1, input, result: 0 },
-	{ part1: 2, input, result: 0 },
+	{ part: 1, input, result: 0 },
+	{ part: 2, input, result: 0 },
 ];
