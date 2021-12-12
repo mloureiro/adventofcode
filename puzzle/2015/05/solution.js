4d749efee34f00823f74913c915ff0265cba6f14
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-12-12 23:19:42 +0100

    Add 2015 day 05 resolution

---
+++ puzzle/2015/05/solution.js
@@ -0,0 +1,24 @@
+export const validation = [];
+
+export const formatInput = input => input.split('\n');
+
+export const part1 = input => {
+	const validate = string => [
+		s => s.replace(/[^aeiou]/g, '').length >= 3,
+		s => s.split('').some((c, idx) => c === s[idx -1]),
+		s => ['ab', 'cd', 'pq', 'xy'].every(v => !s.includes(v))
+	]
+		.every(val => val(string));
+
+	return input.filter(s => validate(s)).length;
+};
+
+export const part2 = input => {
+	const validate = string => [
+		s => /(\w{2}).*\1/.test(s),
+		s => /(\w).\1/.test(s),
+	]
+		.every(val => val(string))
+
+	return input.filter(s => validate(s)).length;
+};
---
+++ puzzle/2015/05/tests.js
@@ -0,0 +1,18 @@
+const input1 = `
+ugknbfddgicrmopn
+aaa
+jchzalrnumimnmhp
+haegwjzuvuyypxyu
+dvszwmarrgswjxmb
+`.trim();
+const input2 = `
+qjhvhtzxzqqjkmpb
+xxyxx
+uurcxstgmygtbstg
+ieodomkazucvgmuy
+`.trim();
+
+export const tests = [
+	{ part: 1, result: 2, input: input1 },
+	{ part: 2, result: 2, input: input2 },
+];
