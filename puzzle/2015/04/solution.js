aed59c51013ff9af5f25ca4dc59c0489a26f7594
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-12-12 22:54:38 +0100

    Add 2015 day 04 resolution

---
+++ puzzle/2015/04/solution.js
@@ -0,0 +1,22 @@
+import md5 from 'crypto-js/md5.js';
+
+export const validation = [];
+
+export const formatInput = input => input;
+
+const findNumber = (secret, initialValue) => {
+	let i = 0;
+	while(true) {
+		if (md5(secret + i).toString().startsWith(initialValue))
+			return i;
+		i++;
+	}
+}
+
+export const part1 = input => {
+	return findNumber(input, '00000');
+};
+
+export const part2 = input => {
+	return findNumber(input, '000000');
+};
---
+++ puzzle/2015/04/tests.js
@@ -0,0 +1,5 @@
+export const tests = [
+	{ part: 1, result: 609043, input: 'abcdef' },
+	{ part: 1, result: 1048970, input: 'pqrstuv' },
+	{ part: 2, result: 0, input: '' },
+];
