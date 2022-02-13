4d705a489511293f2bb4cacee4711ec4453b9d46
Marcos Loureiro <mloureiro.dev@gmail.com>
2022-02-13 14:26:06 +0100

    Add 2015 day 11 resolution

---
+++ puzzle/2015/11/solution.js
@@ -0,0 +1,56 @@
+export const validation = [];
+
+export const formatInput = input => input;
+
+const isSequence = str => str.split('')
+	.every((c, i, s) => {
+		// console.log('>', i, c, s[i - 1], {
+		// 	isFirst: i === 0,
+		// 	code: c.charCodeAt(0),
+		// 	prev: i !== 0 && s[i - 1].charCodeAt(0),
+		// 	isSeq: i !== 0 && c.charCodeAt(0) === 1 + s[i - 1].charCodeAt(0),
+		// })
+		return i === 0 || c.charCodeAt(0) === 1 + s[i - 1].charCodeAt(0);
+	});
+
+const incrementStr = str => {
+	let updatedStr = str.split('');
+	let current = updatedStr.length - 1;
+	let carry = 1;
+	while (carry && current >= 0) {
+		if (updatedStr[current] === 'z') {
+			updatedStr[current] = 'a';
+			carry = 1;
+		} else {
+			updatedStr[current] = String.fromCharCode(updatedStr[current].charCodeAt(0) + carry);
+			carry = 0;
+		}
+		current--;
+	}
+
+	if (carry)
+		updatedStr.unshift('a');
+
+	return updatedStr.join('');
+}
+
+const validCharacters = 'abcdefghijklmnopqrstuvwxyz'.replace(/[iol]/g, '');
+const isPasswordValid = password => Object.values({
+	eightCharachters: p => p.length === 8,
+	validCharacters: p => RegExp(`^[${validCharacters}]*$`).test(p),
+	sequenceOfThree: p => p.split('').some((_, idx) =>
+		idx >= 3 && isSequence(p.slice(idx - 3, idx))),
+	twoNonOverlappingPairs: p => /(.)\1{1}.*(.)\2{1}/.test(p),
+})
+	.every(rule => rule(password));
+
+export const part1 = input => {
+	let password = input;
+	while (!isPasswordValid(password = incrementStr(password)));
+
+	return password;
+};
+
+export const part2 = input => {
+	return part1(part1(input));
+};
---
+++ puzzle/2015/11/tests.js
@@ -0,0 +1,4 @@
+export const tests = [
+	{ part: 1, result: 'abcdffaa', input: 'abcdefgh' },
+	{ part: 2, result: 'abcdffbb', input: 'abcdefgh' },
+];
