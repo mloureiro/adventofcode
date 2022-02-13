e662bb577e9ddaedbe308f5462a775a9bea5ccc9
Marcos Loureiro <mloureiro.dev@gmail.com>
2022-02-13 13:00:01 +0100

    Add 2015 day 10 resolution

---
+++ puzzle/2015/10/solution.js
@@ -0,0 +1,35 @@
+export const validation = [492982, 6989950];
+
+export const formatInput = input => input;
+
+const lookAndSay = sequence => {
+	let count = 1;
+	let previous = null;
+	let result = '';
+	for (let c of sequence) {
+		if (!previous) { // first char
+			previous = c;
+		} else if (c === previous) {
+			count++;
+		} else {
+			result += `${count}${previous}`;
+			previous = c;
+			count = 1;
+		}
+	}
+
+	return result + `${count}${previous}`; // handle last char
+};
+
+export const part1 = (input, customIterations = null) => {
+	const times = customIterations || 40;
+	let sequence = input;
+	for (let i = 0; i < times; i++)
+		sequence = lookAndSay(sequence)
+	return customIterations
+		? sequence // return the sequence if we're running the custom tests
+		: sequence.length;
+};
+
+export const part2 = input =>
+	part1(input, 50).length;
---
+++ puzzle/2015/10/tests.js
@@ -0,0 +1,10 @@
+export const tests = [
+	{ part: 1, result: '11', input: '1', args: [1] },
+	{ part: 1, result: '21', input: '11', args: [1] },
+	{ part: 1, result: '1211', input: '21', args: [1] },
+	{ part: 1, result: '21', input: '1', args: [2] },
+	{ part: 1, result: '1211', input: '11', args: [2] },
+	{ part: 1, result: '111221', input: '21', args: [2] },
+	{ part: 1, result: '312211', input: '11', args: [4] },
+	// { part: 2, result: 0, input },
+];
